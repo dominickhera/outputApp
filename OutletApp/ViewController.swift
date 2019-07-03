66b567f59aed6367abda900508fd6442e3c1510c
Dominick <dominick@finnest.co>
2019-07-03 12:55:04 -0400

    Initial Commit

---
+++ OutletApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OutletApp
+//
+//  Created by Dominick Hera on 7/3/19.
+//  Copyright © 2019 Dominick Hera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
